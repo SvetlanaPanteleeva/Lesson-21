9c2e3712325a464cc02a4a5529bae5b16a225deb
Светлана Пантелеева <panteleevas@gmail.com>
2023-02-18 00:10:16 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static int[,] pole = new int[m, n];
+        static int m;
+        static int n;
+        static void Main(string[] args)
+        {
+            m = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+            pole = new int[m, n];
+            Thread sadovnic1 = new Thread(sad1);
+            Thread sadovnic2 = new Thread(sad2);
+
+            sadovnic1.Start();
+            sadovnic2.Start();
+
+            sadovnic1.Join();
+            sadovnic2.Join();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(pole[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void sad1()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (pole[i, j] == 0)
+                        pole[i, j] = 1;
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
+        private static void sad2()
+        {
+            for (int i = m - 1; i > 0; i--)
+            {
+                for (int j = n - 1; j > 0; j--)
+                {
+                    if (pole[j, i] == 0)
+                        pole[j, i] = 2;
+                    Thread.Sleep(2);
+                }
+            }
+
+        }
+    }
+}
